cf65d53add081810f4b3b43538720c6015cac48b
lucastrschneider <lucastrschneider@gmail.com>
2022-07-17 00:21:14 -0300

    🚧 Try to integrate pipeline stages
    Co-authored-by: guedeslucas1 <guedeslucas1@users.noreply.github.com>
    Co-authored-by: Vanderson Santos <VanderSant@users.noreply.github.com>

--- t_five_pipeline/component/t_five_pipeline.vhd
+++ t_five_pipeline/component/t_five_pipeline.vhd
@@ -57,7 +57,88 @@ architecture structural of t_five_pipeline is
             Q : out std_logic_vector(NB - 1 downto 0)
         );
     end component;
+
+    component fetch is
+        port(
+            clk, reset: in std_logic;
     
+            -- Sinais de controle
+            pc_src     : in std_logic;
+            
+            -- Branches
+            NPCJ       : in    std_logic_vector(31 downto 0);
+    
+            -- Interface com memoria de instrucoes
+            imem_out   : in    std_logic_vector(31 downto 0);
+            imem_add   : out   std_logic_vector(31 downto 0);
+    
+            -- Interface IF/ID
+            IF_ID     : out   std_logic_vector(63 downto 0)
+    
+        );
+    end component;
+    
+    component decode is 
+        port(
+            clk, reset: in std_logic;
+
+            -- Interface IF/ID
+            IF_ID: in std_logic_vector(63 downto 0);
+
+            -- Interface ID/EX
+            ID_EX: out std_logic_vector(138 downto 0);     
+
+            -- Entradas
+            reg_write: in std_logic;
+            rd: in std_logic_vector(4 downto 0);
+            data_write: in std_logic_vector(31 downto 0)
+        );
+    end component;
+
+    component execute is 
+        port(
+            ID_EX : in std_logic_vector(138 downto 0);
+            EX_MEM : out std_logic_vector(139 downto 0)
+        );
+    end component execute;
+    
+    component mem is 
+        port(
+            clk, reset: in std_logic;
+
+            --interface EX/MEM
+            EX_MEM:     in std_logic_vector(139 downto 0);     
+
+            --interface MEM/WB
+            MEM_WB:     out std_logic_vector(70 downto 0);
+
+            -- interface com memória
+            rw:         out std_logic;
+            address:    out std_logic_vector(31 downto 0);
+            data_write: out std_logic_vector(31 downto 0);
+            data_read:  in std_logic_vector(31 downto 0);
+
+            -- interface com fetch
+            NPCJ:       out std_logic_vector(31 downto 0);
+            PCsrc:      out std_logic
+        );
+    end component mem;
+
+    component writeback is 
+    port(
+        clk, reset: in std_logic;
+
+        -- Interface MEM/WB
+        MEM_WB: in std_logic_vector(70 downto 0);  
+
+        -- Saídas
+        reg_write: out std_logic;
+        rd: out std_logic_vector(4 downto 0);
+        data_write: out std_logic_vector(31 downto 0)
+
+    );
+    end component;
+
     -- Sinais internos para memória
     signal m_rw: std_logic;
     signal m_imem_add, m_imem_out, m_dmem_add, m_dmem_out, m_dmem_in: std_logic_vector(31 downto 0);
@@ -69,19 +150,19 @@ architecture structural of t_five_pipeline is
     signal m_mem_wb_d, m_mem_wb_q : std_logic_vector(70 downto 0) := (others => '0');
 
     -- Entradas e saídas entre os estágios
-    signal pc_src : std_logic := '0';
-    signal NPCJ : std_logic_vector(31 downto 0) := (others => '0');
+    signal m_pc_src : std_logic := '0';
+    signal m_NPCJ : std_logic_vector(31 downto 0) := (others => '0');
 
-    signal reg_write : std_logic := '0';
-    signal data_write : std_logic_vector(31 downto 0) := (others => '0');
-    signal rd : std_logic_vector(4 downto 0) := (others => '0');
+    signal m_reg_write : std_logic := '0';
+    signal m_reg_data_write : std_logic_vector(31 downto 0) := (others => '0');
+    signal m_rd : std_logic_vector(4 downto 0) := (others => '0');
 
 begin
 IMEM: rom
     generic map(
-        BE => 30;
-        BP => 32;
-        file_name => "t_five_pipeline/data/mrom.txt";
+        BE => 30,
+        BP => 32,
+        file_name => "t_five_pipeline/data/default_imem.txt",
         Tread => 5 ns
     )    
     port map( 
@@ -92,15 +173,15 @@ IMEM: rom
 
 DMEM: ram
     generic map(
-        BE => 30;
-        BP => 32;
-        file_name => "t_five_pipeline/data/mram.txt";
-        Tz => 2 ns;
-        Twrite => 5 ns;
+        BE => 30,
+        BP => 32,
+        file_name => "t_five_pipeline/data/default_dmem.txt",
+        Tz => 2 ns,
+        Twrite => 5 ns,
         Tread => 5 ns
     )
     port map( 
-        clock => clock,
+        clk => clock,
         reset => reset,
         rw => m_rw,
         ender => m_dmem_add(31 downto 2),
@@ -176,5 +257,57 @@ MEM_WB: reg
         Q => m_mem_wb_q
     );
 
+IF_STAGE: fetch 
+    port map(
+        clk => clock,
+        reset => reset,
+        pc_src => m_pc_src,
+        NPCJ => m_NPCJ,
+        imem_out => m_imem_out,
+        imem_add => m_imem_add,
+        IF_ID => m_if_id_d
+    );
+
+ID_STAGE: decode
+    port map(
+        clk => clock,
+        reset => reset,
+        IF_ID => m_if_id_q,
+        ID_EX =>  m_id_ex_d,
+        reg_write => m_reg_write,
+        rd => m_rd,
+        data_write => m_reg_data_write
+    );
+
+EX_STATE: execute
+    port map(
+        ID_EX => m_id_ex_q,
+        EX_MEM => m_ex_mem_d
+    );
+
+MEM_STAGE: mem
+    port map(
+        clk => clock,
+        reset => reset,
+        EX_MEM => m_ex_mem_q,     
+        MEM_WB => m_mem_wb_d,
+        rw => m_rw,
+        data_write => m_dmem_in,
+        address => m_dmem_add,
+        data_read => m_dmem_out,
+        NPCJ => m_NPCJ,
+        PCsrc => m_pc_src
+    );
+
+
+WB_STAGE: writeback
+    port map(
+        clk => clock,
+        reset => reset,
+        MEM_WB => m_mem_wb_q,
+        reg_write => m_reg_write,
+        rd => m_rd,
+        data_write => m_reg_data_write
+    );
 
 end architecture structural;
--- t_five_pipeline/description.mk
+++ t_five_pipeline/description.mk
@@ -3,10 +3,10 @@
 # 07/2020
 
 # Name of all components in priority order
-CPNT_LIST := txt_util fetch decode execute alu alu_control mux2x1 reg reg_file sign_ext control writeback
+CPNT_LIST := txt_util alu alu_control mux2x1 mux2x1_1b reg reg_file sign_ext control ram rom fetch decode execute mem writeback t_five_pipeline
 
 # Name of the component to be tested
-CPNT ?= execute
+CPNT ?= t_five_pipeline
 
 # Commands to prepare test files
 PREPARE_TEST :=
